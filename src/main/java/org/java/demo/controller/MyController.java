41cebb0fed59a3a75e7f3a3dfe82e8f068685086
sophiamziou <sofiamziou@gmail.com>
2023-05-26 00:19:00 +0200

    exercise done

--- src/main/java/org/java/demo/controller/MyController.java
+++ src/main/java/org/java/demo/controller/MyController.java
@@ -1,7 +1,6 @@
 package org.java.demo.controller;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.java.demo.media.Movie;
@@ -11,13 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class MyController {
 
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String sayHelloToName(Model model) {
 		model.addAttribute("name", "Sophia");
 		return "index";
@@ -27,11 +25,20 @@ public class MyController {
 	
 		List<Movie> bestMovies = new ArrayList<>();
 		
-		for (int i = 1; i < 5; i++) {
-			Movie m = new Movie(i, "Movie" + i);
-			bestMovies.add(m);
-		}
-		
+	    Movie m1 = new Movie(1, "Movie 1");
+	    Movie m2 = new Movie(2, "Movie 2");
+	    Movie m3 = new Movie(3, "Movie 3");
+	    Movie m4 = new Movie(4, "Movie 4");
+	    Movie m5 = new Movie(5, "Movie 5");
+	    Movie m6 = new Movie(6, "Movie 6");
+
+	    bestMovies.add(m1);
+	    bestMovies.add(m2);
+	    bestMovies.add(m3);
+	    bestMovies.add(m4);
+	    bestMovies.add(m5);
+	    bestMovies.add(m6);
+	    
 		return bestMovies;
 		
 	}
@@ -40,71 +47,76 @@ public class MyController {
 		
 		List<Song> bestSongs = new ArrayList<>();
 		
-		for (int i = 1; i < 5; i++) {
-			Song s = new Song(i, "Song" + i);
-			bestSongs.add(s);
-		}
+	    Song s1 = new Song(1, "Song 1");
+	    Song s2 = new Song(2, "Song 2");
+	    Song s3 = new Song(3, "Song 3");
+	    Song s4 = new Song(4, "Song 4");
+	    Song s5 = new Song(5, "Song 5");
+	    Song s6 = new Song(6, "Song 6");
+
+	    bestSongs.add(s1);
+	    bestSongs.add(s2);
+	    bestSongs.add(s3);
+	    bestSongs.add(s4);
+	    bestSongs.add(s5);
+	    bestSongs.add(s6);
 		
 		return bestSongs;
 	}
 	
 	@GetMapping("/movies")
 	public String getMovies(Model model) {
-		
-		String mts = "";
-		Iterator<Movie> it = getBestMovies().iterator();
-		while(it.hasNext()) {
-			mts += it.next().getTitle();
-			
-			if (it.hasNext())
-				mts += ", ";
-		}
-		model.addAttribute("title", mts);
-		return "media";
+	    List<Movie> bestMovies = getBestMovies();
+	    model.addAttribute("medias", bestMovies);
+	    return "movie";
 	}
 	
 	@GetMapping("/songs")
 	public String getSongs(Model model) {
-		
-		String mts = "";
-		Iterator<Song> it = getBestSongs().iterator();
-		while(it.hasNext()) {
-			mts += it.next().getTitle();
-			
-			if (it.hasNext())
-				mts += ", ";
-		}
-		model.addAttribute("title", mts);
-		return "media";
+	    List<Song> bestSongs = getBestSongs();
+	    model.addAttribute("medias", bestSongs);
+	    return "song";
 	}
 	
 	@GetMapping("/movies/{id}")
 	public String getMovie(Model model, @PathVariable("id") int id) {
+		
+		Movie selectedMovie = null;
+	    for (Movie m : getBestMovies()) {
+	        if (m.getId() == id) {
+	            selectedMovie = m;
+	            break;
+	        }
+	    }
 
-		String resT = null;
-		for (Movie m : getBestMovies()) 
-			if (m.getId() == id) 
-				resT = m.getTitle();
+	    if (selectedMovie != null) {
+	        model.addAttribute("media", selectedMovie);
+	    } else {
+	        model.addAttribute("media", "no movie found");
+	    }
 		
-		if (resT != null)
-			model.addAttribute("title", resT);
-		else 
-			model.addAttribute("title", "no movie found");
+		return "detail";
 		
-		return "media";
 	}
 	
 	@GetMapping("/songs/{id}")
 	public String getSong(Model model, @PathVariable("id") int id) {
 		
-		Song resS = null;
-		for (Song s : getBestSongs())
-			if (s.getId() == id)
-				resS = s;
+		Song selectedSong = null;
 		
-		if (resS != null)
-			model.addAttribute("title", resS.getTitle());
+	    for (Song s : getBestSongs()) {
+	        if (s.getId() == id) {
+	            selectedSong = s;
+	            break;
+	        }
+	    }
+
+	    if (selectedSong != null) {
+	        model.addAttribute("media", selectedSong);
+	    } else {
+	        model.addAttribute("media", "no movie found");
+	    }
 		
-		return "media";
+		return "detail";
 	}
 }
